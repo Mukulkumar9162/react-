bc19e22cc33a415a0da05470f427477e0ba9b330
Mukulkumar9162 <mukulkumar1858@gmail.com>
2025-09-03 11:29:04 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Food from './food';
+import './App.css'
+
+const Home = ({ cartData, SetCartData }) => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://dummyjson.com/recipes')
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.recipes, "API Data");
+        setApiData(data.recipes);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Food apiData={apiData} cartData={cartData} SetCartData={SetCartData} />
+    </div>
+  );
+};
+
+export default Home;
